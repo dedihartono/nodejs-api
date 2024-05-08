37267775c656ea6040e53e5ae97045ea93e4b835
Dedi Hartono <dedi.hartono@mad.co.id>
2024-05-08 23:19:53 +0800

    feat(user): user with relation

--- src/sequelize/models/User.ts
+++ src/sequelize/models/User.ts
@@ -41,79 +41,81 @@ export class User extends Model<UserAttributes> implements UserAttributes {
 
 const sequelize = sequelizeConnection;
 
-export function initUserModel(sequelize: Sequelize): void {
-  User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      is_active: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true,
-      },
-      email_verified_at: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-      remember_token: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      created_at: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updated_at: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      deleted_at: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-      created_by: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      updated_by: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      deleted_by: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      sequelize,
-      modelName: 'User',
-      timestamps: false, // Assuming timestamps are handled by Sequelize
-      tableName: 'users', // Optional: define the table name if different from the model name
-      underscored: true, // Optional: use underscored naming for columns (e.g., created_at instead of createdAt)
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-  );
-}
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    email_verified_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    remember_token: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    created_by: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    updated_by: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    deleted_by: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    timestamps: false, // Assuming timestamps are handled by Sequelize
+    tableName: 'users', // Optional: define the table name if different from the model name
+    underscored: true, // Optional: use underscored naming for columns (e.g., created_at instead of createdAt)
+  },
+);
 
-initUserModel(sequelize);
+User.belongsTo(User, { foreignKey: 'created_by', as: 'creator' });
+User.belongsTo(User, { foreignKey: 'updated_by', as: 'modifier' });
+User.belongsTo(User, { foreignKey: 'deleted_by', as: 'remover' });
+
+export default User;
